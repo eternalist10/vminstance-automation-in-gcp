6706923d2855bae0a07cf339f54747d254063dea
Shubhraneel Chakraborty <chakrabortyshubhraneel@gmail.com>
2026-02-17 19:40:33 +0000

    Staged changes

---
+++ vm-instance-automation-with-terraform/variables.tf
@@ -0,0 +1,19 @@
+variable project {
+  type        = string
+  default     = ""
+  description = "Project ID"
+}
+
+variable region {
+  type        = string
+  
+  description = "Region"
+}
+
+variable zone {
+  type        = string
+  default     = ""
+  description = ""
+}
+
+
